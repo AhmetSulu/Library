b5a3c4ba0ddba30d40dacba00b7aa12a80d85c1a
Shock <ahmet.sulu1993@gmail.com>
2025-02-19 16:21:40 +0300

    Finish

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api/v1';
+const BASE_URL = 'https://chilly-dody-shock-995d4b78.koyeb.app/api/v1';
 
 const api = axios.create({
   baseURL: BASE_URL,
